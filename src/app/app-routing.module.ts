342b05569bed3781ffa7cb39fd2f258bd445b95c
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2023-05-26 10:33:53 +0200

    Add routing : - Route leads to sign-up & user - Default display user content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  { path: '', component: UserComponent, pathMatch: 'full' },
+  { path: 'sign-up', component: SignupComponent, pathMatch: 'full' },
+  { path: 'user', component: UserComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
